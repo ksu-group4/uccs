5f66967f16a7aff23cb69bec51bea5929dfb7b44
phoerrle <phoerrle@kent.edu>
2015-03-29 14:46:58 -0400

    Create equipment.cpp
    added equipment.cpp

---
+++ equipment.cpp
@@ -0,0 +1,10 @@
+/*
+equipment.cpp
+Declaration file for the class equipment.
+*/
+
+#include "equipment.h"
+
+/*
+Equipment is an item used for a presentation that is reservered for a session. 
+*/
