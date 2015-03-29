608e2929eeb006c63d94debd191dcbf0b0fe76f0
phoerrle <phoerrle@kent.edu>
2015-03-29 15:10:02 -0400

    Create guest.h
    initial guest.h template

---
+++ guest.h
@@ -0,0 +1,20 @@
+/*
+guest.h
+Declaration file for the class guest.
+
+Guest represents a visitor to the conference.
+*/
+
+#ifndef INCLUDED_GUEST_H
+#define INCLUDED_GUEST_H
+
+using namespace std;
+
+class guest {
+public:
+
+private:
+
+};
+
+#endif
