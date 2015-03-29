0dde13557e5674ff85c6f2b214818e0a797302d9
phoerrle <phoerrle@kent.edu>
2015-03-29 14:51:37 -0400

    Create room.cpp
    added initial room.cpp template

---
+++ room.cpp
@@ -0,0 +1,10 @@
+/*
+room.cpp
+Declaration file for the class room.
+*/
+
+#include "room.h"
+
+/*
+A room is an area to reserve, if available, to hold a conference. There are three different sized rooms, each with a different price. 
+*/
