2f1f6db88463deb743dcc9aad859f0dcf38b119d
phoerrle <phoerrle@kent.edu>
2015-03-29 14:54:31 -0400

    Create conference.h
    initial conference template

---
+++ conference.h
@@ -0,0 +1,20 @@
+/*
+conference.h
+Declaration file for the class conference.
+
+A conference is a meeting for consultation, discussion or to settle disputes of significant matter.
+*/
+
+#ifndef INCLUDED_CONFERENCE_H
+#define INCLUDED_CONFERENCE_H
+
+using namespace std;
+
+class conference {
+public:
+
+private:
+
+};
+
+#endif
