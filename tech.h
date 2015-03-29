a55d74d20f1e8bf229b51732a8ccce886c7a1f6c
phoerrle <phoerrle@kent.edu>
2015-03-29 14:36:14 -0400

    Create tech.h
    added tech.h file

---
+++ tech.h
@@ -0,0 +1,18 @@
+/*
+   tech.h
+   Declaration file for the class tech.
+*/
+
+#ifndef INCLUDED_TECH_H
+#define INCLUDED_TECH_H
+
+using namespace std;
+
+class tech {
+public:
+
+private:
+
+};
+
+#endif
