0a032362c9fd1ceb43f450f0d078853d75109e31
phoerrle <phoerrle@kent.edu>
2015-03-29 14:43:07 -0400

    Create equipment.h
    added equipment.h

---
+++ equipment.h
@@ -0,0 +1,20 @@
+/*
+tech.h
+
+Declaration file for the class tech.
+Equipment is an item used for a presentation that is reservered for a session. 
+*/
+
+#ifndef INCLUDED_EQUIPMENT_H
+#define INCLUDED_EQUIPMENT_H
+
+using namespace std;
+
+class equipment {
+public:
+
+private:
+
+};
+
+#endif
