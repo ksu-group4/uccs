425062b6cacf1f8f2a93f1e6107b34463670f153
phoerrle <phoerrle@kent.edu>
2015-03-29 15:00:03 -0400

    Create session.h
    initial session.h template

---
+++ session.h
@@ -0,0 +1,20 @@
+/*
+session.h
+Declaration file for the class session.
+
+A Session is a single sitting for conference of those assembled
+*/
+
+#ifndef INCLUDED_SESSION_H
+#define INCLUDED_SESSION_H
+
+using namespace std;
+
+class session {
+public:
+
+private:
+
+};
+
+#endif
