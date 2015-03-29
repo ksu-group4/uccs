2d11ba5531cf0b90e37b969262e92d95f93c23f4
phoerrle <phoerrle@kent.edu>
2015-03-29 14:57:47 -0400

    Create conference.cpp
    initial conference.cpp template

---
+++ conference.cpp
@@ -0,0 +1,10 @@
+/*
+conference.cpp
+Declaration file for the class conference.
+*/
+
+#include "conference.h"
+
+/*
+A conference is a meeting for consultation, discussion or to settle disputes of significant matter.
+*/
