596ef180a65e637fbfe4a5589f99657686c6f236
phoerrle <phoerrle@kent.edu>
2015-03-29 14:40:02 -0400

    Create tech.cpp
    added tech.cpp

---
+++ tech.cpp
@@ -0,0 +1,12 @@
+/*
+  tech.cpp
+
+  Declaration file for the class tech.
+*/
+
+#include "tech.h"
+
+/*
+  A Technician is a qualified person that attends each session to provide technical support and to monitor/report on the   status of each piece of equipment in attendence; replace equipment if required. 
+*/
+
