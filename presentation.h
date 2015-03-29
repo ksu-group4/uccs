674d789d94c5f4de00b7c8eb155b8f237326cfcc
phoerrle <phoerrle@kent.edu>
2015-03-29 15:07:13 -0400

    Create presentation.h
    initial presentation.h template

---
+++ presentation.h
@@ -0,0 +1,20 @@
+/*
+presentation.h
+Declaration file for the class presentation.
+
+A Presentation is a demonstration or lecture.
+*/
+
+#ifndef INCLUDED_PRESENTATION_H
+#define INCLUDED_PRESENTATION_H
+
+using namespace std;
+
+class presentation {
+public:
+
+private:
+
+};
+
+#endif
