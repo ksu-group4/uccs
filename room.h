577e88a7688e636ae6d0123132f25ef8fbee973f
phoerrle <phoerrle@kent.edu>
2015-03-29 14:48:59 -0400

    Create room.h
    added room.h initial template

---
+++ room.h
@@ -0,0 +1,20 @@
+/*
+room.h
+Declaration file for the class room.
+
+A room is an area to reserve, if available, to hold a conference. There are three different sized rooms, each with a different price.
+*/
+
+#ifndef INCLUDED_ROOM_H
+#define INCLUDED_ROOM_H
+
+using namespace std;
+
+class room {
+public:
+
+private:
+
+};
+
+#endif
