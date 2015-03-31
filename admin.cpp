dd3226d5ba62cbfbae3a236fad499f2f8b1a682c
phoerrle <phoerrle@kent.edu>
2015-03-31 11:46:57 -0400

    Create admin.cpp
    draft admin.cpp template

---
+++ admin.cpp
@@ -0,0 +1,11 @@
+
+/*
+admin.cpp
+Declaration file for the class admin.
+*/
+
+#include "admin.h"
+
+/*
+An Administrator is a user with privileges to access and edit protected information that is restricted to other users.
+*/
