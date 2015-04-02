5ef77a3442f5b71caba5f164212e87263307af73
jspahlin <jspahlin@kent.edu>
2015-04-02 16:46:41 -0400

    Create person.h

---
+++ person.h
@@ -0,0 +1,16 @@
+/*
+person.h
+Declaration file for the class guest.
+Guest represents a visitor to the conference.
+*/
+#ifndef INCLUDED_GUEST_H
+#define INCLUDED_GUEST_H
+
+class person {
+public:
+private:
+  int id;
+  std::string name;
+
+};
+#endif
