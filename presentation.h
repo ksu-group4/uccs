44af325cf70124d818711f13ca98d08f993902e2
jspahlin <jspahlin@kent.edu>
2015-04-02 16:51:15 -0400

    Update presentation.h

--- presentation.h
+++ presentation.h
@@ -7,14 +7,19 @@ A Presentation is a demonstration or lecture.
 
 #ifndef INCLUDED_PRESENTATION_H
 #define INCLUDED_PRESENTATION_H
+#include <string>
 
-using namespace std;
+typedef session* session_ref; /// Type decleration for refering to a session.
+class session; // farward declare session class.
 
 class presentation {
 public:
 
 private:
-
+    int id;
+    session_ref session;
+    std::string name;
+    bool is_paid; /// does this presentation require payment?
 };
 
 #endif
