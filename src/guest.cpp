e0732f8644140c332b503565c970424dcf6dd87c
nickterra <nkt693@yahoo.com>
2015-04-08 23:31:44 -0400

    Update guest.cpp
    Updated code. Prompts the user to enter guest names and stores those names in a queue.

--- src/guest.cpp
+++ src/guest.cpp
@@ -1,11 +1,44 @@
+//Nicholas Terra
+//4/8/15
+//Declaration file for the class guest.
+//guest represents a visitor to the conference.
 
-/*
-guest.cpp
-Declaration file for the class guest.
-*/
-
+#include <iostream>
+#include <string>
+#include <queue>
 #include "guest.h"
 
-/*
-guest represents a visitor to the conference.
-*/
+using namespace std;
+
+int main()  
+{
+
+again ='y';
+
+while(again == 'y' || again == 'Y')
+	{
+	  cout << "Enter name of guest...\n";
+	    cin >> ws;
+		getline (cin, name);
+
+		q.push(name);
+
+	  cout << "Welcome " << name << "! \n"
+	       << "Enter a new guest? (y/n) \n";
+			   cin >> again;
+	}	
+
+	cout << endl;
+
+	cout << "There are " << q.size() << " guests currently in this queue." << endl << endl;
+
+	while(!q.empty())
+	{
+		cout << "\t" << q.front() << endl;
+		q.pop();
+		cout << endl;
+	}   
+
+system ("pause");
+return 0;
+}
