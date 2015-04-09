a91d63e1e5fd010e3e5d35dca1f5bf4e97d67072
nickterra <nkt693@yahoo.com>
2015-04-08 23:33:15 -0400

    Update guest.h
    updated the header file

--- src/guest.h
+++ src/guest.h
@@ -5,16 +5,13 @@ Declaration file for the class guest.
 Guest represents a visitor to the conference.
 */
 
-#ifndef INCLUDED_GUEST_H
-#define INCLUDED_GUEST_H
-
+#ifndef _GUEST_H
+#define _GUEST_H
 using namespace std;
 
-class guest {
-public:
-
-private:
-
-};
+#include <string>
+queue<string> q;
+string name;
+char again;
 
 #endif
