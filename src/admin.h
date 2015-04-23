6d720dd778458d2dadd9d1954ba94f10968bc2e1
nickterra <nkt693@yahoo.com>
2015-04-23 14:39:24 -0400

    Update admin.h
    updated admin.h

--- src/admin.h
+++ src/admin.h
@@ -4,15 +4,14 @@ Declaration file for the class admin
 
 An Administrator is a user with privileges to access and edit protected information that is restricted to other users.
 */
-#ifndef INCLUDED_ADMIN_H
-#define INCLUDED_ADMIN_H
 
+#ifndef _ADMIN_H
+#define _ADMIN_H
 using namespace std;
 
-class conference {
-public:
+#include <string>
+queue<string> q;
+string name;
+char again;
 
-private:
-
-};
 #endif
