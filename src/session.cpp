8949c66525ad465cbb283a371561b3451501871c
nickterra <nkt693@yahoo.com>
2015-04-26 21:26:08 -0400

    Update session.cpp

--- src/session.cpp
+++ src/session.cpp
@@ -8,3 +8,26 @@ Declaration file for the class session.
 /*
 A Session is a single sitting for conference of those assembled
 */
+/**
+
+session::session()
+{
+ //default constructor
+}
+
+session::~session()
+{
+ //default deconstructor
+}
+
+void session::addSession()
+{
+    //Function to add new session
+
+}
+
+void session::removeSession()
+{
+    //function to remove new session
+}
+
