b1b0d456e187af076e949de4027ad16ee967aaf5
phoerrle <phoerrle@kent.edu>
2015-04-26 20:06:48 -0400

    Update equipment.h
    added to header

--- src/equipment.h
+++ src/equipment.h
@@ -8,13 +8,25 @@ Equipment is an item used for a presentation that is reservered for a session.
 #ifndef INCLUDED_EQUIPMENT_H
 #define INCLUDED_EQUIPMENT_H
 
+#include <iostream>
+#include <string>
+#include <queue>
+#include <cstdlib>
+
 using namespace std;
 
 class equipment {
 public:
-
+  equipment();
+  bool is_broken();
+  void list_equip();
+  void assign_equip();
+  queue<string> equipment_names;
+  queue<string> room_names;
 private:
-
+  bool broken = false;
+  string eq_name;
+  string room_name;
 };
 
 #endif
